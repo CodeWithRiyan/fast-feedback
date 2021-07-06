74fb7d1325f614a0a6821ed10adad802e4492148
Muhamad Riyan <riyan@skyshi.com>
2021-07-06 19:21:43 +0700

    finish part2

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,39 @@
-import { ProvideAuth } from '../lib/auth';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import { Global, css } from '@emotion/core';
+import { AuthProvider } from '../lib/auth';
+import customTheme from '@/styles/theme';
 
-function MyApp({ Component, pageProps }) {
+const GlobalStyle = ({ children }) => {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
   );
-}
+};
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+};
 
 export default MyApp;
